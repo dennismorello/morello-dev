dfb6183b46806ab6c07c6d04e8044dfe8301a6e5
dennismorello <dennis.morello@contentwise.com>
2020-12-23 15:49:38 +0100

    fix(sections): section headings are now colored; fixed Safari scroll-margin-top CSS rule

--- src/components/section-with-heading/section-with-heading.component.js
+++ src/components/section-with-heading/section-with-heading.component.js
@@ -1,17 +1,29 @@
 import { Container, Heading, Link, useBreakpointValue } from "@chakra-ui/react";
 
-const SectionWithHeading = ({ id, title, css, children, ...otherProps }) => {
-  const scrollMarginTop = useBreakpointValue([16, null, 96]);
+const SectionWithHeading = ({
+  id,
+  title,
+  titleProps,
+  css,
+  children,
+  ...otherProps
+}) => {
+  const scrollMarginTop = useBreakpointValue([72, null, 96]);
 
   return (
     <Container
       as="section"
       maxW="6xl"
       id={id}
-      css={{ scrollMarginTop, ...css }}
+      css={{
+        scrollMarginTop,
+        paddingTop: scrollMarginTop,
+        marginTop: -scrollMarginTop,
+        ...css,
+      }}
       {...otherProps}
     >
-      <Heading as="h2" size="lg">
+      <Heading as="h2" size="lg" color="teal.300" {...titleProps}>
         {id ? <Link href={`#${id}`}>{title}</Link> : title}
       </Heading>
       {children}
