ac81bfe0465d54ddf62b88436ec34216abdcfc05
dennismorello <dennis.morello@contentwise.com>
2020-12-23 13:50:36 +0100

    fix(seo): fixed SEO issues

--- src/next-seo.config.js
+++ src/next-seo.config.js
@@ -6,12 +6,12 @@ const defaultSeo = {
   openGraph: {
     type: "website",
     locale: "en_US",
-    url: process.env.VERCEL_URL,
+    url: "https://morello.dev",
     site_name: "Dennis Morello",
     title: "Dennis Morello",
     images: [
       {
-        url: "https://morello.dev/images/profile.png",
+        url: "https://morello.dev/images/twitter-card.png",
         width: 400,
         height: 400,
         alt: "Dennis Morello",
@@ -27,7 +27,7 @@ const defaultSeo = {
 const socialProfileJsonLd = {
   type: "Person",
   name: "Dennis Morello",
-  url: process.env.VERCEL_URL,
+  url: "https://morello.dev",
   sameAs: [
     "https://twitter.com/dennismorello",
     "https://instagram.com/dennismrl",
