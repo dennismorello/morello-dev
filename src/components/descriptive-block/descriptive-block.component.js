3d5880648afadc133fc78b796fc99c453eba35b4
dennismorello <dennis.morello@contentwise.com>
2020-12-23 14:52:05 +0100

    fix(html): fixed html headings ordering

--- src/components/descriptive-block/descriptive-block.component.js
+++ src/components/descriptive-block/descriptive-block.component.js
@@ -2,11 +2,11 @@ import { Box, Heading, VStack } from "@chakra-ui/react";
 
 const DescriptiveBlock = ({ title, subtitle, children, ...otherProps }) => (
   <VStack align="flex-start" {...otherProps}>
-    <Heading as="h5" size="md" textAlign="left">
+    <Heading as="h3" size="md" textAlign="left">
       {title}
     </Heading>
     <Heading
-      as="h6"
+      as="h4"
       size="sm"
       textAlign="left"
       textTransform="none"
